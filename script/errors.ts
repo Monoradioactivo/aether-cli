a676234e7c0c02bd0f31ee3b513f4335cdc2938d
Adrián Moreno <adrian.moreno.94@outlook.com>
2026-08-30 22:57:15 -0600

    fix(cli): print required scopes on 403 errors (#116)

--- script/errors.ts
+++ script/errors.ts
@@ -2,13 +2,21 @@ export class AetherError extends Error {
   public readonly statusCode: number;
   public readonly requestId?: string;
   public readonly code?: string;
+  public readonly requiredScopes?: string[];
 
-  constructor(message: string, statusCode: number, requestId?: string, code?: string) {
+  constructor(
+    message: string,
+    statusCode: number,
+    requestId?: string,
+    code?: string,
+    requiredScopes?: string[]
+  ) {
     super(message);
     this.name = "AetherError";
     this.statusCode = statusCode;
     this.requestId = requestId;
     this.code = code;
+    this.requiredScopes = requiredScopes;
     Object.setPrototypeOf(this, AetherError.prototype);
   }
 }
